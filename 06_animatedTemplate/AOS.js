41674db4d8f1a889d4cde444dd9f6a7c93c60b92
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-19 01:41:43 +0530

    added external script file

---
+++ 06_animatedTemplate/AOS.js
@@ -0,0 +1,8 @@
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+
+  delay: 200, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
